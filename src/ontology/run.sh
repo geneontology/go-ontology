014d3d306128bae74ebfdd8e9d9a10aacf4a418c
Eric Douglass <edouglass@lbl.gov>
2018-08-10 19:19:53 -0700

    timing for each step, using correct docker image, updating travis config

--- src/ontology/run.sh
+++ src/ontology/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -m 12g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti cmungall/oort  "$@"
+docker run -m 12g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull  "$@"
