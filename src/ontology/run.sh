8f791de17778dfe095ec6010c3a6b36de7317cbc
Jim Balhoff <jim@balhoff.org>
2019-02-26 08:36:17 -0500

    Update to latest odk.

--- src/ontology/run.sh
+++ src/ontology/run.sh
@@ -2,4 +2,4 @@
 
 # When updating the odkfull version, remember to also update the .travis.yml file.
 
-docker run -m 12g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull:v1.1.7  "$@"
+docker run -m 12g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull:v1.2.3 "$@"
