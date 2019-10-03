845ac66f2931d75448f9e389beed3ba0890c7549
Jim Balhoff <jim@balhoff.org>
2019-10-03 16:33:40 -0400

    Add SOP about odkfull coordination

--- src/ontology/run.sh
+++ src/ontology/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Any updates to the odkfull version MUST be coordinated with geneontology/pipeline.
 # When updating the odkfull version, remember to also update the .travis.yml file.
 
 docker run -m 12g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull:v1.2.18 "$@"
