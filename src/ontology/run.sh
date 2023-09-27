2b405ddd0db0770222a70814b6e4adabcb5adbdc
Jim Balhoff <jim@balhoff.org>
2023-09-27 09:14:43 -0400

    Build computed taxon constraints as part of go-plus.

--- src/ontology/run.sh
+++ src/ontology/run.sh
@@ -3,4 +3,4 @@
 # Any updates to the odkfull version MUST be coordinated with geneontology/pipeline.
 # When updating the odkfull version, remember to also update the GitHub Actions workflows.
 
-docker run -m 12g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull:v1.4 "$@"
+docker run -m 32g  -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull:v1.4 "$@"
